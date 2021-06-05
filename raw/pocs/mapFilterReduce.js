d81096289fdf5d2266c243c9c57f33d8d3c0c587
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-05 17:42:29 +0530

    Initial Commit

---
+++ raw/pocs/mapFilterReduce.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5,6,7];
+
+//to twice eles of this arrray to a new array
+
+let doubleArr = [];
+for(let i = 0; i < arr.length; i++){
+    doubleArr[i] = arr[i]*2;
+}
+
+console.log(doubleArr);
