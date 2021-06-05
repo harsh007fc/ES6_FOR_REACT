d414c6fa80d5a67aa651b27c5140348548eb91ba
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-05 18:13:40 +0530

    added upto filter

--- raw/pocs/mapFilterReduce.js
+++ raw/pocs/mapFilterReduce.js
@@ -24,7 +24,7 @@
 
 ///////////////Filter/////////////////////////////
 // used to filter according to our need
-let words =  ['mera','tera','sabka','hamara'];
+// let words =  ['mera','tera','sabka','hamara'];
 // let fArr = []; 
 // for(let i = 0; i < words.length; i++){
 //     let  word = words[i];
@@ -38,8 +38,30 @@ let words =  ['mera','tera','sabka','hamara'];
 
 // Returns the elements of an array that meet the condition specified in a callback function.//
 
-let filter = function(word){
-    return word.length > 4;
-}
-let fArr = words.filter(filter);
-console.log(fArr);
+// let filter = function(word){
+//     return word.length > 4;
+// }
+// let fArr = words.filter(filter);
+// console.log(fArr);
+
+
+
+////////////////////////REDUCE///////////////////////
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let sum = 0;
+// for(let i = 0; i  < arr.length; i++){
+//     sum+=arr[i];
+// }
+// console.log(sum);
+
+
+
+
+// Calls the specified callback function for all the elements in an array. The return value of the callback function is the accumulated result, and is provided as an argument in the next call to the callback function.
+// let sum = arr.reduce(function(acc,cVal){
+//     console.log('acc->'+acc+" "+ 'cVal->'+cVal);
+//     return acc+cVal;
+// });
+// console.log(sum);
+
