677fc3c50e9286044da98bf8699eaad35473834f
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-05 17:52:13 +0530

    added upto map

--- raw/pocs/mapFilterReduce.js
+++ raw/pocs/mapFilterReduce.js
@@ -2,9 +2,21 @@ let arr = [1,2,3,4,5,6,7];
 
 //to twice eles of this arrray to a new array
 
-let doubleArr = [];
-for(let i = 0; i < arr.length; i++){
-    doubleArr[i] = arr[i]*2;
-}
+// let doubleArr = [];
+// for(let i = 0; i < arr.length; i++){
+//     doubleArr[i] = arr[i]*2;
+// }
 
-console.log(doubleArr);
+// console.log(doubleArr);
+
+
+// map function is a heigher order function
+//that either accepts a function as an argument
+// or returns a function 
+
+let df = function(el){
+    return 2 * el;
+}
+let darr = arr.map(df);
+console.log(darr);
+console.log(arr);
