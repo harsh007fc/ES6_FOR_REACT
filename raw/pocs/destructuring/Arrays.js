b782ae5d0900b0752c26c1913cea4649172041a6
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-06 15:33:51 +0530

    upto spread operator

--- raw/pocs/destructuring/Arrays.js
+++ raw/pocs/destructuring/Arrays.js
@@ -25,13 +25,23 @@ let newArr = ['hello'];
 // console.log(second); // =>undefined
 //TO AVOID ABOVE UNDEFINED CONDITION
 
-let [first,second='second'] = newArr;
-console.log(first);
-console.log(second);
+// let [first,second='second'] = newArr;
+// console.log(first);
+// console.log(second);
 
 
-let a = 5;
-let b = 6;
-[a,b] = [b,a];
-console.log(a);
-console.log(b);
+// let a = 5;
+// let b = 6;
+// [a,b] = [b,a];
+// console.log(a);
+// console.log(b);
+
+
+////SPREAD OPERATOR THREE DOTS
+
+let parr = [1,2,3,4,5,6,7,8,9];
+let [fe,se,te,...narr] = parr;
+console.log(fe);
+console.log(se);
+console.log(te);
+console.log(narr);
