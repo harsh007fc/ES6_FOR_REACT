7b8e86c3439c4cb6b0c0af16bbab3f4659e17728
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-06 12:00:52 +0530

    upto destructuring

--- raw/pocs/destructuring/Arrays.js
+++ raw/pocs/destructuring/Arrays.js
@@ -1,9 +1,37 @@
 let arr = ['I','am','harsh'];
+/////////FIRST
 // let first = arr[0];
 // let second = arr[1];
 // let third = arr[2];
 
-let [first,second,third] = arr;
+
+///////SECOND
+// let [first,second,third] = arr;
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+
+//////if we want to (SKIP) an elem
+// let [first,,third] = arr;
+// console.log(first);
+// console.log(third);
+
+
+let newArr = ['hello'];
+
+// let [first,second] = newArr;
+// console.log(first); //=>hello
+// console.log(second); // =>undefined
+//TO AVOID ABOVE UNDEFINED CONDITION
+
+let [first,second='second'] = newArr;
 console.log(first);
 console.log(second);
-console.log(third);
+
+
+let a = 5;
+let b = 6;
+[a,b] = [b,a];
+console.log(a);
+console.log(b);
