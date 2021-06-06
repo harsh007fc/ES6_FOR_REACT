ceb85ee5689adfa965f47c08e1e2ea3cd1f92455
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-06 11:51:46 +0530

    DESTRUCTURING ADDED

---
+++ raw/pocs/destructuring/Arrays.js
@@ -0,0 +1,9 @@
+let arr = ['I','am','harsh'];
+// let first = arr[0];
+// let second = arr[1];
+// let third = arr[2];
+
+let [first,second,third] = arr;
+console.log(first);
+console.log(second);
+console.log(third);
