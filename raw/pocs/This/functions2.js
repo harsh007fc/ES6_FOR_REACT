ceb9e44bd739b765984f12da4b9bc8e295a48cd6
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-08 12:43:41 +0530

    added upto bind

---
+++ raw/pocs/This/functions2.js
@@ -0,0 +1,52 @@
+
+
+
+// function fn(){
+//      console.log(this.person);
+//      console.log(this);
+//      function abc(){
+//          console.log(this)
+//          console.log(this.person);
+//      }
+//      abc();
+//  }
+
+//  let obj = {
+//      person:'harsh',
+//      func:fn
+//  }
+
+//  obj.func();
+
+
+ //to get my name when abc() is called
+ //we have 2 solns
+ //1st solutions using bind==>
+ // let ret = functioni.bind(argument)
+// the bind method returns another function whose this is equal to the argument that is passed to the bind funcion
+
+ function fn(){
+    console.log(this.person);
+    console.log(this);
+    function abc(){
+        console.log(this)
+        console.log(this.person);
+    }
+   //  abc()l
+   let ret = abc.bind(this);
+   //the value of this ret function is == to this i.e. obj
+   ret();
+   abc();
+}
+
+let obj = {
+    person:'harsh',
+    func:fn
+}
+
+obj.func();
+
+
+//second soln is arrow function
+
+ 
