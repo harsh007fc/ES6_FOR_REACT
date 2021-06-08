edfa139f6d4f4eafaee6cfa0340ff6f9507493c2
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-08 13:08:22 +0530

    arrow done

--- raw/pocs/This/functions2.js
+++ raw/pocs/This/functions2.js
@@ -25,18 +25,43 @@
  // let ret = functioni.bind(argument)
 // the bind method returns another function whose this is equal to the argument that is passed to the bind funcion
 
- function fn(){
+//  function fn(){
+//     console.log(this.person);
+//     console.log(this);
+//     function abc(){
+//         console.log(this)
+//         console.log(this.person);
+//     }
+//    //  abc()l
+//    let ret = abc.bind(this);
+//    //the value of this ret function is == to this i.e. obj
+//    ret();
+//    abc();
+// }
+
+// let obj = {
+//     person:'harsh',
+//     func:fn
+// }
+
+// obj.func();
+
+
+//second soln is arrow function
+
+// let fon = (arguments)=>{
+//     statements
+// }
+//this of arrow fnt is equal to this of parent function or lexically superior function (bahr ka this hi andr ka ho jata hai)
+
+function fn(){
     console.log(this.person);
     console.log(this);
-    function abc(){
-        console.log(this)
+    let abc = ()=>{
+        console.log(this);
         console.log(this.person);
     }
-   //  abc()l
-   let ret = abc.bind(this);
-   //the value of this ret function is == to this i.e. obj
-   ret();
-   abc();
+    abc();
 }
 
 let obj = {
@@ -46,7 +71,5 @@ let obj = {
 
 obj.func();
 
-
-//second soln is arrow function
 
  
